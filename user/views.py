fc1c7cb70efacd6ea35540c74d2105ea7e1bcf1e
eseme <132669119+esemekluivert@users.noreply.github.com>
2023-09-05 15:15:10 +0100

    Add files via upload

---
+++ user/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
+from .forms import CreateUserForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib import messages
+
+# Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            group = Group.objects.get(name='Customers')
+            user.groups.add(group)
+            messages.success(request, "user registration was succesfull.")
+            return redirect('user-login')
+    else:
+        form = CreateUserForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'user/register.html', context)
+
+@login_required
+def profile(request):
+    context = {
+
+    }
+    return render(request, 'user/profile.html', context)
+
+login_required
+def profile_update(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, "Profile details updated succesfully.")
+
+            return redirect('user-profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+    return render(request, 'user/profile_update.html', context)
